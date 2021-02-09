26e7313f44bd95c2014c9693605963ddc6628518
Jose Miguel C Evangelista <jomigevangelista117@gmail.com>
2021-02-09 14:04:46 +0800

    Push to github -initial commit

---
+++ ifStatement.js
@@ -0,0 +1,23 @@
+const id = 100;
+
+// Equal to 
+if(id == 100) {
+    console.log('Correct');
+} else {
+    console.log ('Incorrect');
+}
+
+// Not Equal 
+if(id != 101) {
+  console.log('Correct')  
+} else {
+    console.log('Incorrect');
+
+}
+
+// Equal to value and type
+if(id == 100) {
+    console.log('Correct');
+} else {
+    console.log('Incorrect')
+}
